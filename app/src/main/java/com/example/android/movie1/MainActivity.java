720b1aac7527b9f7c1f742369305cd54f7e9c654
neohuang <superneohuang@gmail.com>
2017-04-17 14:21:43 +0200

    Add Menus

--- app/src/main/java/com/example/android/movie1/MainActivity.java
+++ app/src/main/java/com/example/android/movie1/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.android.movie1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.movie, menu);
+        return true;
     }
 }
