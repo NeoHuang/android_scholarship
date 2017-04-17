2d2bdc23daaa8dba83c63bab9922a8179a6a5cc9
neohuang <superneohuang@gmail.com>
2017-04-17 22:20:06 +0200

    Setup the movie db key

--- app/src/main/java/com/example/android/movie1/MainActivity.java
+++ app/src/main/java/com/example/android/movie1/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.movie1;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -9,11 +11,13 @@ import android.view.MenuItem;
 import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG="Movie1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.v(TAG, BuildConfig.MOVIE_DB_KEY);
     }
 
     @Override
@@ -22,4 +26,5 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.movie, menu);
         return true;
     }
+
 }
